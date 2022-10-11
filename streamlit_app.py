8ae4b1ae7adef2d6b441c5a140db53e4f5b56d93
orvarw <115560752+orvarw@users.noreply.github.com>
2022-10-11 15:51:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
 #My app for streamlit thinngys
 
 import streamlit
+import pandas as pd
+
 streamlit.title("Smackkkkkkk bro")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.header("Here we have my header")
 streamlit.text("here we have some text")
 streamlit.text("here we have another text")
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
