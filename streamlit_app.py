5a58418385665819b68daf8fcf3b87991b8bfeeb
orvarw <115560752+orvarw@users.noreply.github.com>
2022-10-11 15:52:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ streamlit.header("Here we have my header")
 streamlit.text("here we have some text")
 streamlit.text("here we have another text")
 
-#my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 #streamlit.dataframe(my_fruit_list)
 
 # Let's put a pick list here so they can pick the fruit they want to include 
