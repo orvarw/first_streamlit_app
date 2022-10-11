9ca9b7da0e082bab18b73eec5bd5a4d4b0f03f99
orvarw <115560752+orvarw@users.noreply.github.com>
2022-10-11 15:48:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@
 
 import streamlit
 streamlit.title("Smackkkkkkk bro")
+streamlit.header("Here we have my header")
+streamlit.text("here we have some text")
+streamlit.text("here we have another text")
