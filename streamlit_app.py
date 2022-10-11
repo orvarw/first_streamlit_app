9912e8791532e9e4e25754e299e41cbe1857eb04
orvarw <115560752+orvarw@users.noreply.github.com>
2022-10-11 15:49:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 
 import streamlit
 streamlit.title("Smackkkkkkk bro")
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.header("Here we have my header")
 streamlit.text("here we have some text")
 streamlit.text("here we have another text")
